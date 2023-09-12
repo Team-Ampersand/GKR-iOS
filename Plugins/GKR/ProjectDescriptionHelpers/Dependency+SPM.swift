7c9996b7b1c5a33dec7c87209129b28941850e1a
Sunghun Kim <81547954+kimsh153@users.noreply.github.com>
2023-09-12 23:35:48 +0900

    :heavy_plus_sign: 외부 라이브러리 주입

--- Plugins/GKR/ProjectDescriptionHelpers/Dependency+SPM.swift
+++ Plugins/GKR/ProjectDescriptionHelpers/Dependency+SPM.swift
@@ -1,9 +1,18 @@
 import ProjectDescription
 
 public extension TargetDependency {
-    enum SPM {}
+    struct SPM {}
 }
 
 public extension TargetDependency.SPM {
-    
+    static let RxSwift = TargetDependency.external(name: "RxSwift")
+    static let RxCocoa = TargetDependency.external(name: "RxCocoa")
+    static let RxMoya = TargetDependency.external(name: "RxMoya")
+    static let SnapKit = TargetDependency.external(name: "SnapKit")
+    static let Swinject = TargetDependency.external(name: "Swinject")
+    static let RxFlow = TargetDependency.external(name: "RxFlow")
+    static let Then = TargetDependency.external(name: "Then")
+    static let ReactorKit = TargetDependency.external(name: "ReactorKit")
+    static let RxDataSources = TargetDependency.external(name: "RxDataSources")
+    static let GAuthSignin = TargetDependency.external(name: "GAuthSignin")
 }
--- Projects/ThirdPartyLib/Project.swift
+++ Projects/ThirdPartyLib/Project.swift
@@ -1,9 +1,22 @@
 import ProjectDescription
 import ProjectDescriptionHelpers
+import MyPlugin
 
 let project = Project.makeModule(
     name: "ThirdPartyLib",
     product: .framework,
     packages: [],
-    dependencies: []
+    deploymentTarget: .iOS(targetVersion: "15.0", devices: .iphone),
+    dependencies: [
+        .SPM.Then,
+        .SPM.RxCocoa,
+        .SPM.RxDataSources,
+        .SPM.RxFlow,
+        .SPM.RxMoya,
+        .SPM.RxSwift,
+        .SPM.SnapKit,
+        .SPM.Swinject,
+        .SPM.ReactorKit,
+        .SPM.GAuthSignin
+    ]
 )
